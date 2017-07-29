84e9b8cff3ae2e648a6489eb8c150907d7a322a8
Michael West <mpwest929@gmail.com>
2017-07-29 11:59:24 -0400

    Implement GameManager and collision detection

--- src/js/pacman.js
+++ src/js/pacman.js
@@ -1,4 +1,122 @@
 // =========================== BEGIN SpriteRepository ======================
+class GameManager {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.objects = {};
+  }
+
+  register(gameObject, key) {
+    this.objects[key] = gameObject;
+  }
+
+  renderWorld(ctx) {
+    for (let key in this.objects) {
+      let object = this.objects[key];
+      object.render(ctx);
+    }
+  }
+
+  checkForCollision(gameObjectOneKey, gameObjectTwoKey) {
+    let gameObjectOne = this.objects[gameObjectOneKey];
+    let gameObjectTwo = this.objects[gameObjectTwoKey];
+
+    if (!(gameObjectOne && gameObjectTwo)) {
+      console.log(`[checkForCollision] Warning: Game object with key of either '${gameObjectOneKey}' or '${gameObjectTwoKey} does not exist.'`);
+      return;
+    }
+
+    let bbOne = gameObjectOne.boundingBox(); // must be a scalar
+
+    if (bbOne.constructor !== BoundingBox) {
+      console.log("[checkForCollision] Warning: bbOne must be an instance of BoundingBox.");
+      return;
+    }
+
+    let bbTwo = gameObjectTwo.boundingBox(); // can be an array of BoundingBox
+
+    let bbTwoArr;
+    if (bbTwo.constructor === Array) {
+      bbTwoArr = bbTwo;
+    } else {
+      bbTwoArr = [bbTwo];
+    }
+
+    for (let bbTwoIndex = 0; bbTwoIndex < bbTwoArr.length; bbTwoIndex++) {
+      let bbTwo = bbTwoArr[bbTwoIndex];
+
+      // Check if BoundingBox is invalid so we don't get a false positive.
+      if (isNaN(bbTwo.x) || isNaN(bbTwo.y) || isNaN(bbTwo.width) || isNaN(bbTwo.height)) {
+        continue;
+      }
+
+      if (this.checkBoundingBoxCollision(bbOne, bbTwo)) {
+        console.log(bbTwo);
+        ctx.fillStyle = 'green';
+        ctx.fillRect(bbTwo.x, bbTwo.y, bbTwo.width, bbTwo.height);
+
+        ctx.fillStyle = 'purple';
+        ctx.fillRect(bbOne.x, bbOne.y, bbOne.width, bbOne.height);
+
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  checkBoundingBoxCollision(bbOne, bbTwo) {
+    let xLeftOne = bbOne.x;
+    let xLeftTwo = bbTwo.x;
+    let xRightOne = bbOne.x + bbOne.width;
+    let xRightTwo = bbTwo.x + bbTwo.width;
+
+    let yTopOne = bbOne.y;
+    let yTopTwo = bbTwo.y;
+    let yBottomOne = bbOne.y + bbOne.height;
+    let yBottomTwo = bbTwo.y + bbTwo.height;
+
+    if (xRightOne < xLeftTwo) {
+      return false;
+    }
+
+    if (xLeftOne > xRightTwo) {
+      return false;
+    }
+
+    if (yBottomOne < yTopTwo) {
+      return false;
+    }
+
+    if (yTopOne > yBottomTwo) {
+      return false;
+    }
+
+    //if (a.max.x < b.min.x) return false; // a is left of b
+    //if (a.min.x > b.max.x) return false; // a is right of b
+    //if (a.max.y < b.min.y) return false; // a is above b
+    //if (a.min.y > b.max.y) return false; // a is below b
+
+    return true; // boxes overlap
+  }
+}
+
+/*let GameObject = {
+  render: (context) => {
+    throw "Error: 'render' is not implemented";
+  }
+}*/
+
+class BoundingBox {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+}
+
+// Represents a subsquare within a spritesheet
+// Arg 'sprite' is the spritesheet
 class Tile {
   constructor(sprite, startX, startY, tileWidth, tileHeight) {
     this.sprite = sprite;
@@ -70,14 +188,25 @@ class Sprite {
 }
 
 class SpriteRepository {
-  constructor(spritePaths) {
+  constructor(spritePaths, loadedCallback) {
     this.sprites = [];
+    this.loadedCallback = loadedCallback;
 
     spritePaths.forEach((path) => {
       let spriteKey = this.extractName(path);
       this.sprites[spriteKey] = new Sprite();
       this.sprites[spriteKey].image.src = path;
     });
+  }
+
+  isLoaded() {
+    for (let spriteKey in this.sprites) {
+      if (!this.sprites[spriteKey].loaded) {
+        return false;
+      }
+    };
+
+    return true;
   }
 
   fetch(key) {
@@ -95,53 +224,120 @@ class SpriteRepository {
 
 // ============================== BEGIN Grid =============================
 class Grid {
-  constructor(gridTiles) {
+  constructor(gridTiles, baseX, baseY) {
+    this.tileWidth = 32;
+    this.tileHeight = 32;
+
+    this.baseX = baseX;
+    this.baseY = baseY;
+
     this.tiles = {
-      "ULC": gridTiles[6],    // upper left corner
-      "HTA": gridTiles[7],   // horizontal top aligned
-      "URC": gridTiles[21],  // upper right corner
-      "VLA": gridTiles[36],  // vertical left aligned
-      "VRA": gridTiles[51],  // vertical right aligned
-      "BNK": gridTiles[5],   // blank
-      "SLR": gridTiles[68],  // small lower right corner
-      "SLL": gridTiles[69],  // small lower left corner
-      "MHL": gridTiles[73],  // middle horizontal line
-      "RVL": gridTiles[98],  // right vertical line
-      "LVL": gridTiles[99],  // left vertical line
-      "SUR": gridTiles[101],  // small upper right corner
-      "UHL": gridTiles[102], // upper horizontal line
-      "SUL": gridTiles[106],   // small upper left corner
-      "LLC": gridTiles[930],   // lower left corner
-      "LRC": gridTiles[957],   // lower right corner
-      "HBA": gridTiles[931],   // horizontal bottom aligned
+      "1": gridTiles[6],    // upper left corner
+      "2": gridTiles[7],   // horizontal top aligned
+      "3": gridTiles[21],  // upper right corner
+      "4": gridTiles[36],  // vertical left aligned
+      "5": gridTiles[51],  // vertical right aligned
+      "0": gridTiles[5],   // blank
+      "6": gridTiles[68],  // small lower right corner
+      "7": gridTiles[69],  // small lower left corner
+      "8": gridTiles[73],  // middle horizontal line
+      "9": gridTiles[98],  // right vertical line
+      "10": gridTiles[99],  // left vertical line
+      "11": gridTiles[101],  // small upper right corner
+      "12": gridTiles[102], // upper horizontal line
+      "13": gridTiles[106],   // small upper left corner
+      "14": gridTiles[930],   // lower left corner
+      "15": gridTiles[957],   // lower right corner
+      "16": gridTiles[931],   // horizontal bottom aligned
     }
 
     this.grid = [
-      ["BNK", "ULC", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "HTA", "URC", "BNK"],
-      ["BNK", "VLA", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "VRA", "BNK"],
-      ["BNK", "VLA", "BNK", "SLR", "SLL", "BNK", "SLR", "MHL", "MHL", "MHL", "MHL", "MHL", "SLL", "BNK", "SLR", "SLL", "BNK", "VRA", "BNK"],
-      ["BNK", "VLA", "BNK", "LVL", "RVL", "BNK", "LVL", "BNK", "BNK", "BNK", "BNK", "BNK", "RVL", "BNK", "LVL", "RVL", "BNK", "VRA", "BNK"],
-      ["BNK", "VLA", "BNK", "LVL", "RVL", "BNK", "LVL", "BNK", "BNK", "BNK", "BNK", "BNK", "RVL", "BNK", "LVL", "RVL", "BNK", "VRA", "BNK"],
-      ["BNK", "VLA", "BNK", "SUR", "SUL", "BNK", "SUR", "UHL", "UHL", "UHL", "UHL", "UHL", "SUL", "BNK", "SUR", "SUL", "BNK", "VRA", "BNK"],
-      ["BNK", "VLA", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "BNK", "VRA", "BNK"],
-      ["BNK", "LLC", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "HBA", "LRC", "BNK"],
+      ["0", "1", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "3", "0"],
+      ["0", "4", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "5", "0"],
+      ["0", "4", "0", "6", "7", "0", "6", "8", "8", "8", "8", "8", "7", "0", "6", "8", "7", "0", "5", "0"],
+      ["0", "4", "0", "10", "9", "0", "10", "0", "0", "0", "0", "0", "9", "0", "10", "0", "9", "0", "5", "0"],
+      ["0", "4", "0", "10", "9", "0", "10", "0", "0", "0", "0", "0", "9", "0", "10", "0", "9", "0", "5", "0"],
+      ["0", "4", "0", "11", "13", "0", "11", "12", "12", "12", "12", "12", "13", "0", "11", "12", "13", "0", "5", "0"],
+      ["0", "4", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "5", "0"],
+      ["0", "14", "16", "16", "16", "16", "16", "16", "16", "16", "16", "16", "16", "16", "16", "16", "16", "16", "15", "0"],
     ];
+
+    this.tileWidth = 32;
+    this.tileHeight = 32;
+  }
+
+  iterateOverTiles(eachFn) {
+    let currX = this.baseX, currY = this.baseY;
+
+    this.grid.forEach((row, rowIndex) => {
+      row.forEach((tile, colIndex) => {
+        eachFn(tile, currX, currY);
+
+        currX += this.tileWidth;
+      });
+
+      currX = this.baseX;
+      currY += this.tileHeight;
+    });
+  }
+
+  // Returns an array of bounding boxes
+  boundingBox() {
+    let boxes = [];
+
+    this.iterateOverTiles((tile, currX, currY) => {
+     // empty tiles should not be included
+      if (tile !== "0") {
+        let bb;
+
+        if (tile === "2") {
+          bb = new BoundingBox(currX, currY, this.tileWidth, 0.5 * this.tileHeight);
+        } else if (tile === "4") {
+          bb = new BoundingBox(currX, currY, 0.5 * this.tileWidth, this.tileHeight);
+        } else if (tile === "5") {
+          bb = new BoundingBox(
+            currX + 0.5 * this.tileWidth,
+            currY,
+            0.5 * this.tileWidth,
+            this.tileHeight
+          );
+        } else if (tile === "6") {
+          bb = new BoundingBox(
+            currX + 0.5 * this.tileWidth,
+            currY + 0.5 * this.tileHeight,
+            0.5 * this.tileWidth,
+            0.5 * this.tileHeight
+          );
+        } else if (tile === "10") {
+          bb = new BoundingBox(
+            this.currX + 0.5 * this.tileWidth,
+            this.currY,
+            0.5 * this.tileWidth,
+            this.tileHeight
+          );
+        } else {
+          bb = new BoundingBox(currX, currY, this.tileWidth, this.tileHeight);
+        }
+
+        boxes.push(bb);
+      }
+    });
+
+    return boxes;
   }
 
   render(context) {
-    let startX = 0, startY = 0;
-    let currX = startX, currY = startY;
-    let tileWidth = 32, tileHeight = 32;
+    let currX = this.baseX, currY = this.baseY;
 
-    this.grid.forEach((row) => {
-      row.forEach((tile) => {
+    this.grid.forEach((row, rowIndex) => {
+      row.forEach((tile, colIndex) => {
         this.tiles[tile].renderAt(ctx, currX, currY);
 
-        currX += tileWidth;
+        currX += this.tileWidth;
       });
 
-      currX = startX;
-      currY += tileHeight;
+      currX = this.baseX;
+      currY += this.tileHeight;
     });
   }
 }
@@ -152,7 +348,8 @@ const DirectionState = {
   UP: "UP",
   DOWN: "DOWN",
   LEFT: "LEFT",
-  RIGHT: "RIGHT"
+  RIGHT: "RIGHT",
+  STILL: "STILL"
 };
 
 class Frame {
@@ -167,34 +364,30 @@ class Frame {
 }
 
 class Animation {
-  constructor(frames) {
+  constructor(frames, shouldLoop) {
     this.frames = frames;
     this.currentFrameIndex = 0;
     this.ANIMATION_COMPLETED = -1;
     this.currentTimer = null;
+    this.shouldLoop = shouldLoop;
   }
 
-  playOnce(speed) {
+  play(speed) {
     this.currentTimer = setInterval(() => {
-      this.currentFrameIndex += 1;
-
-      if (this.currentFrameIndex === this.frames.length) {
-        this.currentFrameIndex = this.ANIMATION_COMPLETED;
+      if (this.shouldLoop) {
+        this.currentFrameIndex = (this.currentFrameIndex + 1) % this.frames.length;
+      } else {
+        this.currentFrameIndex += 1;
+        if (this.currentFrameIndex === this.frames.length) {
+          this.currentFrameIndex = this.ANIMATION_COMPLETED;
+        }
       }
-    }, speed);
-  }
-
-  playLoop(speed) {
-    this.currentTimer = setInterval(() => {
-      this.currentFrameIndex = (this.currentFrameIndex + 1) % this.frames.length;
     }, speed);
   }
 
   stop() {
     if (this.currentTimer) {
       window.clearInterval(this.currentTimer);
-      this.currentTimer = null;
-      this.currentFrameIndex = this.ANIMATION_COMPLETED;
     }
   }
 
@@ -216,86 +409,118 @@ class Player {
 
     let leftOpen = new Frame([tiles[192], tiles[193], tiles[224], tiles[225]], renderFn);
     let leftClose = new Frame([tiles[196], tiles[197], tiles[228], tiles[229]], renderFn);
-    this.leftAnim = new Animation([leftOpen, leftClose]);
+    this.leftAnim = new Animation([leftOpen, leftClose], true);
 
     let rightOpen = new Frame([tiles[200], tiles[201], tiles[232], tiles[233]], renderFn);
     let rightClose = new Frame([tiles[204], tiles[205], tiles[236], tiles[237]], renderFn);
-    this.rightAnim = new Animation([rightOpen, rightClose]);
+    this.rightAnim = new Animation([rightOpen, rightClose], true);
 
     let upOpen = new Frame([tiles[194], tiles[195], tiles[226], tiles[227]], renderFn);
     let upClose = new Frame([tiles[198], tiles[199], tiles[230], tiles[231]], renderFn);
-    this.upAnim = new Animation([upOpen, upClose]);
+    this.upAnim = new Animation([upOpen, upClose], true);
 
     let downOpen = new Frame([tiles[202], tiles[203], tiles[234], tiles[235]], renderFn);
     let downClose = new Frame([tiles[206], tiles[207], tiles[238], tiles[239]], renderFn);
-    this.downAnim = new Animation([downOpen, downClose]);
+    this.downAnim = new Animation([downOpen, downClose], true);
 
     this.x = x;
     this.y = y;
+
+    this.moveUnitIncrement = 2;
     this.moveX = 0;
     this.moveY = 0;
 
-    this.right();
+    this.directionState = DirectionState.STILL;
+    this.animation = this.rightAnim;
+  }
+
+  boundingBox() {
+    return new BoundingBox(this.x, this.y, 64, 64);
   }
 
   left() {
+    if (this.directionState === DirectionState.LEFT) {
+      return;
+    }
+
     if (this.animation) {
       this.animation.stop();
     }
 
     this.directionState = DirectionState.LEFT;
     this.animation = this.leftAnim;
-    this.animation.playLoop(125);
+    this.animation.play(125);
 
-    this.moveX = -10;
+    this.moveX = -this.moveUnitIncrement;
     this.moveY = 0;
   }
 
   right() {
+    if (this.directionState === DirectionState.RIGHT) {
+      return;
+    }
+
     if (this.animation) {
       this.animation.stop();
     }
 
     this.directionState = DirectionState.RIGHT;
     this.animation = this.rightAnim;
-    this.animation.playLoop(125);
+    this.animation.play(125);
 
-    this.moveX = 10;
+    this.moveX = this.moveUnitIncrement;
     this.moveY = 0;
   }
 
   up() {
+    if (this.directionState === DirectionState.UP) {
+      return;
+    }
+
     if (this.animation) {
       this.animation.stop();
     }
 
     this.directionState = DirectionState.UP;
     this.animation = this.upAnim;
-    this.animation.playLoop(125);
+    this.animation.play(125);
 
     this.moveX = 0;
-    this.moveY = -10;
+    this.moveY = -this.moveUnitIncrement;
   }
 
   down() {
+    if (this.directionState === DirectionState.DOWN) {
+      return;
+    }
+
     if (this.animation) {
       this.animation.stop();
     }
 
     this.directionState = DirectionState.DOWN;
     this.animation = this.downAnim;
-    this.animation.playLoop(125);
+    this.animation.play(125);
 
     this.moveX = 0;
-    this.moveY = 10;
+    this.moveY = this.moveUnitIncrement;
   }
 
-  update() {
-   this.x += this.moveX;
-   this.y += this.moveY;
+  update(gameManager) {
+    if (gameManager.checkForCollision("player", "grid")) {
+      console.log("COLLISION DETECTED BETWEEN PLAYR AND GRID");
+    }
+
+    this.x += this.moveX;
+    this.y += this.moveY;
   }
 
   render(context) {
+    // render Player boundingBox
+    //let playerBb = player.boundingBox();
+    //ctx.fillStyle = 'green';
+    //ctx.fillRect(playerBb.x, playerBb.y, playerBb.width, playerBb.height);
+
     this.animation.render(context, this.x, this.y);
   }
 }
@@ -317,16 +542,19 @@ debug.collision = false;
 
 let spritesRepo = new SpriteRepository([
   'src/img/grid.png',
-  'src/img/sprites.png'
+  'src/img/sprites-alpha.png'
 ]);
 
-// 216 = 47 + 6x
-// 23 + 6(32 + 4)
 let gridTiles = spritesRepo.fetch("grid").asTiles(24, 24, 32, 32, 4);
-let grid = new Grid(gridTiles);
+let grid = new Grid(gridTiles, 0, 0);
 
-let generalSprites = spritesRepo.fetch("sprites").asTiles(1, 1, 47, 47, 1);
-let player = new Player(generalSprites, 700, 100);
+let generalSprites = spritesRepo.fetch("sprites-alpha").asTiles(1, 1, 47, 47, 1);
+
+let player = new Player(generalSprites, 50, 20);
+
+let gameManager = new GameManager(ctx);
+gameManager.register(grid, "grid");
+gameManager.register(player, "player");
 
 let keys = {}
 let processKeyDownEvent = (e) => {
@@ -362,9 +590,8 @@ let renderBackground = () => {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 };
 
-
 let update = () => {
-  player.update();
+  player.update(gameManager);
 
   if (keys['up']) {
     player.up();
@@ -381,21 +608,33 @@ let update = () => {
 }
 
 let render = () => {
-  renderBackground();
-  grid.render(ctx);
-  player.render(ctx);
+  if (spritesRepo.isLoaded()) {
+    renderBackground();
+
+    gameManager.renderWorld(ctx);
+  }
+}
+
+let computeFps = (currentTime) => {
+  let fpsAsFloat = 1000 / (currentTime - prevTime);
+
+  return Math.trunc(fpsAsFloat + 0.5);
+}
+
+let reportFps = (fps) => {
+  let fpsElement = document.getElementById("fps");
+  fpsElement.innerHTML = `Frames Per Second = ${fps}`;
 }
 
 let prevTime = performance.now();
 let main = () => {
   let currentTime = performance.now();
-  let fps = 1000 / (currentTime - prevTime);
+  let fps = computeFps(currentTime);
   prevTime = currentTime;
+  reportFps(fps);
 
-  console.log(fps);
-
-  update();
   render();
+  update();
 
   window.requestAnimationFrame(main);
 }
